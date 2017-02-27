366da4a212c537dcf1fa9c5835a3e65d544318df
Dan <hellodanhett@gmail.com>
2017-02-27 20:14:35 +0000

    Added color wipe

--- WirelessController/WirelessController.ino
+++ WirelessController/WirelessController.ino
@@ -39,7 +39,8 @@ void loop() {
     GREEN  = Serial.parseInt();
     WHITE = Serial.parseInt();
     if (Serial.read() == '\n') {
-      colourAll(RED, BLUE, GREEN, WHITE);
+      //colourAll(RED, BLUE, GREEN, WHITE);
+      colorWipe(RED, BLUE, GREEN, WHITE, 100);
       Serial.println("R: " + String(RED) + " G:" + String(BLUE) + " B:" + String(GREEN) + " Brightness:" + String(WHITE));
     }
   }
@@ -51,5 +52,13 @@ void colourAll(int r, int g, int b, int w) {
     strip.setPixelColor(i, r, g, b, w);
   }
   strip.show();
+}
+
+void colorWipe(int r, int g, int b, int w, uint8_t wait) {
+  for(uint16_t i=0; i<NUMPIXELS; i++) {
+    strip.setPixelColor(i, r, g, b, w);
+    strip.show();
+    delay(wait);
+  }
 }
 
