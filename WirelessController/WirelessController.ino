897bd7ce9416d6923ddbf9a5de7017ab4c36654c
jrmedd <hello@jamesmedd.co.uk>
2017-03-03 13:08:12 +0000

    Add header identifcation in wireless controller code

--- WirelessController/WirelessController.ino
+++ WirelessController/WirelessController.ino
@@ -8,16 +8,18 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+int thisDevice = 1; //change according to device (1-5)
+int globalDevice = 0;
+
 char colorValues[NUMPIXELS*3];
 int RED;
 int GREEN;
 int BLUE;
-int WHITE;
 int bright;
 
 void setup() {
   bright = 128;
-  
+
   // Init the NeoPixel library and turn off all the LEDs
   strip.setBrightness(bright);
   strip.begin();
@@ -35,17 +37,18 @@ void setup() {
   Serial.println("READY");
 }
 
+//We're expecting a message like '1,255,128,75' from the Processing (device,r,g,b)
 
 void loop() {
   if (Serial.available() > 0) {
+    int header = Serial.parseInt();
     RED  = Serial.parseInt();
     GREEN  = Serial.parseInt();
     BLUE = Serial.parseInt();
     if (Serial.read() == '\n') {
-      colourAll(RED, GREEN, BLUE);
-      
-      //colorWipe(RED, GREEN, BLUE, SPEED);
-      //Serial.println("R: " + String(RED) + " G:" + String(GREEN) + " B:" + String(BLUE));
+      if (header == thisDevice || header == globalDevice) {
+        colourAll(RED, GREEN, BLUE);
+      }
     }
   }
 }
@@ -57,13 +60,3 @@ void colourAll(int r, int g, int b) {
   }
   strip.show();
 }
-
-// OLD
-void colorWipe(int r, int g, int b, uint8_t wait) {
-  for(uint16_t i=0; i<NUMPIXELS; i++) {
-    strip.setPixelColor(i, r, g, b);
-    strip.show();
-    delay(wait);
-  }
-}
-
